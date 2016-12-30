a0b456d1a3536a6dfb3f72eec89751a77966f458
Jaemin Han <jaemin.han@nyu.edu>
2016-12-29 21:40:20 -0500

    entry point initial setup complete

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    filename: "build/bundle.js",
+    sourceMapFilename: "build/bundle.map"
+  },
+  devtool: '#source-map',
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        exclude: /(node_modules)/,
+        query:{
+          presets:['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
+
+// jsx - html directly inside moduele
